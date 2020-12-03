f020564fbcb982487eaf7b39330f835057db84d5
iliana etaoin <iliana@buttslol.net>
2020-12-02 20:35:06 -0800

    day 2 part 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,60 +1,61 @@
 #![no_std]
 
 use aoc2020::*;
-use core::str::FromStr;
 
+#[derive(Debug)]
 struct Input {
-    min: u8,
-    max: u8,
-    count: u8,
+    a: usize,
+    b: usize,
+    letter: char,
+    password: &'static str,
 }
 
 impl Input {
-    fn load(s: &str) -> Option<Input> {
+    fn from_str(s: &'static str) -> Option<Input> {
         let minus = s.find('-')?;
         let space = s.find(' ')?;
         let colon = s.find(": ")?;
 
-        let min: u8 = s[0..minus].parse().ok()?;
-        let max: u8 = s[(minus + 1)..space].parse().ok()?;
-        let letter = s[(space + 1)..].chars().next()?;
+        let a = s[0..minus].parse().ok()?;
+        let b = s[(minus + 1)..space].parse().ok()?;
+        let letter = s[(space + 1)..colon].chars().next()?;
+        let password = &s[(colon + 2)..];
+
         Some(Input {
-            min,
-            max,
-            count: s[(colon + 2)..]
-                .chars()
-                .map(|c| if c == letter { 1 } else { 0 })
-                .sum(),
+            a,
+            b,
+            letter,
+            password,
         })
     }
 }
 
-impl FromStr for Input {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Input, ()> {
-        Input::load(s).ok_or(())
-    }
+fn part1<I: Iterator<Item = Input>>(iter: I) -> usize {
+    iter.filter(|i| {
+        let count = i.password.chars().filter(|c| *c == i.letter).count();
+        i.a <= count && count <= i.b
+    })
+    .count()
 }
 
-fn part1<I: Iterator<Item = Input>>(iter: I) -> u16 {
-    iter.map(|i| {
-        if i.min <= i.count && i.count <= i.max {
-            1
-        } else {
-            0
-        }
+fn part2<I: Iterator<Item = Input>>(iter: I) -> usize {
+    iter.filter(|i| {
+        let a = i.password.chars().nth(i.a - 1).unwrap();
+        let b = i.password.chars().nth(i.b - 1).unwrap();
+        (a == i.letter) ^ (b == i.letter)
     })
-    .sum()
+    .count()
 }
 
 #[test]
 fn test() {
     let lines = test_input!(Input);
-    assert_eq!(part1(lines), 2);
+    assert_eq!(part1(lines.clone()), 2);
+    assert_eq!(part2(lines), 1);
 }
 
 fn main() {
     let lines = input!(Input);
-    libc_println!("part 1: {}", part1(lines));
+    libc_println!("part 1: {}", part1(lines.clone()));
+    libc_println!("part 2: {}", part2(lines));
 }
