b37321fb1980ba7c789bf338dbeb95bd641991be
iliana etaoin <iliana@buttslol.net>
2020-12-04 21:59:52 -0800

    day 5 part 2

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -2,6 +2,8 @@
 
 use aoc2020::*;
 use core::ops::Range;
+use heapless::{consts::U1024, Vec};
+use itertools::Itertools;
 
 fn seat_id(s: &str) -> u16 {
     let mut row = 0..128;
@@ -40,8 +42,13 @@ fn test() {
 }
 
 fn main() {
-    libc_println!(
-        "part 1: {}",
-        input!().lines().map(|l| seat_id(l)).max().unwrap()
-    );
+    let mut seats: Vec<u16, U1024> = input!().lines().map(|l| seat_id(l)).collect();
+    seats.sort_unstable();
+    libc_println!("part 1: {}", seats[seats.len() - 1]);
+    for (a, b) in seats.iter().tuple_windows() {
+        if b - a == 2 && (8..1016).contains(&(b - 1)) {
+            libc_println!("part 2: {}", b - 1);
+            break;
+        }
+    }
 }
