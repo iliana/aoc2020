69fa02c5a0d673e4d4f996fe442b8bb351eb837d
iliana etaoin <iliana@buttslol.net>
2020-12-05 22:33:00 -0800

    day 6 part 2

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -17,11 +17,29 @@ fn part1(input: &str) -> usize {
         .sum()
 }
 
+fn part2(input: &str) -> usize {
+    input
+        .split("\n\n")
+        .map(|group| {
+            let mut buf = [0; 26];
+            let lines = group.lines().count();
+            for c in group.bytes() {
+                if c.is_ascii_lowercase() {
+                    buf[(c - b'a') as usize] += 1;
+                }
+            }
+            buf.iter().filter(|c| **c == lines).count()
+        })
+        .sum()
+}
+
 #[test]
 fn test() {
     assert_eq!(part1(input!()), 11);
+    assert_eq!(part2(input!()), 6);
 }
 
 fn main() {
     libc_println!("part 1: {}", part1(input!()));
+    libc_println!("part 2: {}", part2(input!()));
 }
